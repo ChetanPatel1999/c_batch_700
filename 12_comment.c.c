0b246c16ea0c1dcba469910985893019e344b1ad
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-16 21:03:03 +0530

    16/07

---
+++ 12_comment.c.c
@@ -0,0 +1,15 @@
+//Write a program to print square of given number. 
+#include<stdio.h>
+void main()
+{
+	printf("hello students\n");   //some write here
+//	printf("how are you ?\n");
+}
+
+//this is single line comment
+
+/* hello this
+is multiline
+comment   */
+
+
